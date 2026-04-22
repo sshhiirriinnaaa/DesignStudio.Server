128fda8fd73a77700c0206c9403ae79a4c9ca6da
shirina <sshhiirriinnaa@mail.ru>
2026-04-22 19:36:44 +0300

    Добавлена функция полного удаления проектов из базы данных

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -5,39 +5,49 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DesignStudio.Server.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
     public class ProjectsController : ControllerBase
     {
-        
         private readonly AppDbContext _context;
 
-      
         public ProjectsController(AppDbContext context)
         {
             _context = context;
         }
 
- 
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
         {
-         
             return await _context.Projects.ToListAsync();
         }
 
-    
         [HttpPost]
         public async Task<ActionResult<Project>> PostProject(Project project)
         {
-           
             _context.Projects.Add(project);
-         
             await _context.SaveChangesAsync();
 
-         
             return Ok(project);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+          
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+               
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+          
+            return NoContent();
         }
     }
 }
---
+++ Migrations/20260422150007_AddNewFields.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesignStudio.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
